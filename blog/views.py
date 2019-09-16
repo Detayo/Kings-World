1014b72c14b3d446391d486db22bab90bf539215
Adetayo <detayoking@gmail.com>
2019-09-16 21:38:31 +0100

    Changed the HTML and view for site.

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,8 @@ from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 def post_list(request):
-	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	paginate_by=5
 	return render(request, 'blog/post_list.html', {'posts': posts})
 	
 def post_detail(request, pk):
