13eec0bfa7ac886e37f1a6188ff9b0e93b9f4bcb
Adetayo <detayoking@gmail.com>
2019-09-17 16:17:06 +0100

    General Update

--- blog/models.py
+++ blog/models.py
@@ -2,21 +2,23 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import reverse
+
 
 class Post(models.Model):
-	title=models.CharField(max_length=100)
-	text = models.TextField()
-	created_date = models.DateTimeField(default=timezone.now)
-	published_date=models.DateTimeField(default=timezone.now)
-	cover = models.ImageField(upload_to='images/',  default='logo.jpg')
-	author=models.ForeignKey(User, on_delete=models.CASCADE)
-	def __str__(self):
-		return self.title
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    cover = models.ImageField(upload_to='images/',  default='logo.jpg')
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-	def get_absolute_url(self):
-		return reverse('post-detail', kwargs={'pk':self.pk})
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
+    def __str__(self):
+        return self.title
+        
         
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
